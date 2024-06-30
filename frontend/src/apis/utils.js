73587ead48d01aecb118c318b6fc0e3e4cf5665b
Asimwe Landry <landryasimwe@gmail.com>
2024-06-30 03:46:03 +0200

    api changed

--- frontend/src/apis/utils.js
+++ frontend/src/apis/utils.js
@@ -1,18 +1,26 @@
 import axios from "axios";
 
-const apiUrl = "https://mernstack-manager.vercel.app/api";
+// base API URL
+const apiUrl = "https://mernstack-manager.vercel.app/api/v1/tasks";
 
+// API requests
 const makeApiRequest = async (endpoint, options = {}) => {
   const url = `${apiUrl}/${endpoint}`;
-  const response = await axios.request({
-    url,
-    ...options,
-    headers: {
-      ...options.headers,
-      "Content-Type": "application/json",
-    },
-  });
-  return response.data;
+
+  try {
+    const response = await axios.request({
+      url,
+      ...options,
+      headers: {
+        ...options.headers,
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("API request error:", error);
+    throw error.response ? error.response.data : new Error("API request failed");
+  }
 };
 
 export { apiUrl, makeApiRequest };
